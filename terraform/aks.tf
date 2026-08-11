01b0c9b19a03483a0e629ff169a33669037f7de3
TienVCHE191697 <cttien021105@gmail.com>
2026-08-11 15:08:03 +0700

    Update create aks

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -10,20 +10,70 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name = "systempool"
 
-    vm_size    = "Standard_B2s"
-    node_count = 1
-
+    vm_size        = "Standard_D2s_v3"
+    node_count     = 1
+    os_sku         = "Ubuntu"
     vnet_subnet_id = azurerm_subnet.aks.id
 
     only_critical_addons_enabled = true
   }
 
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+
   identity {
-    type = "SystemAssigned"
+    type = "UserAssigned"
+
+    identity_ids = [
+      azurerm_user_assigned_identity.aks.id
+    ]
   }
 
   node_provisioning_profile {
-    os_sku = "Ubuntu"
+    mode = "Manual"
   }
-  
+
+  role_based_access_control_enabled = true
+
+  azure_active_directory_role_based_access_control {
+    azure_rbac_enabled = true
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+
+    load_balancer_sku = "standard"
+
+    service_cidr   = "172.16.0.0/16"
+    dns_service_ip = "172.16.0.10"
+  }
 }
+
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name = "userpool"
+
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+
+  vm_size = "Standard_D2s_v3"
+
+  vnet_subnet_id = azurerm_subnet.aks.id
+
+  mode = "User"
+
+  auto_scaling_enabled = true
+
+  min_count = 1
+  max_count = 5
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope = azurerm_container_registry.acr.id
+
+  role_definition_name = "AcrPull"
+
+  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+
+
--- terraform/identity.tf
+++ terraform/identity.tf
@@ -1,1 +1,5 @@
-# identity.tf
+resource "azurerm_user_assigned_identity" "aks" {
+  name                = "id-aks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
